cc60d53582c97ed98070f1faa4001d86b5be6234
Alessandro Balducci <aleb2000@hotmail.com>
2023-08-06 22:07:40 +0200

    Added meta viewport tag

--- trunk-template-8589831daa20bef9.js
+++ trunk-template-8589831daa20bef9.js
@@ -949,7 +949,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('trunk-template-8d426dafb27656c2_bg.wasm', import.meta.url);
+        input = new URL('trunk-template-8589831daa20bef9_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
